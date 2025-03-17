abf0871fc00841bc445bcc0f219e527ebbdac1a5
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-03-17 08:20:53 +0530

    all added

---
+++ src/constants/socials.tsx
@@ -0,0 +1,23 @@
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandTwitter,
+} from "@tabler/icons-react";
+
+export const socials = [
+  {
+    href: "https://twitter.com/thedevkrish",
+    label: "Twitter",
+    icon: IconBrandTwitter,
+  },
+  {
+    href: "https://github.com/krishna-paulraj",
+    label: "Github",
+    icon: IconBrandGithub,
+  },
+  {
+    href: "https://linkedin.com/in/suresh-krishna-paulraj",
+    label: "LinkedIn",
+    icon: IconBrandLinkedin,
+  },
+];
