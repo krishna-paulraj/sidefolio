4d17198de94932a31c694cb9fd9fe8011607ecb2
krishna-paulraj <krishnapaulraj2004@gmail.com>
2025-03-20 23:01:39 +0530

    fix google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <Script
-          strategy="afterInteractive"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4709921382444878"
           crossOrigin="anonymous"
